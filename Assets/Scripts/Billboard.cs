6c8accda79c1d9fc5ee3a7d7a741be1210339249
AdrianKut <adrian.kut96@onet.pl>
2025-02-25 18:21:09 +0100

    Update Billboard.cs

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -11,7 +11,8 @@ public class Billboard : MonoBehaviour
 
     private void LateUpdate()
     {
-        transform.LookAt( transform.position + m_Camera.transform.rotation * Vector3.forward ); 
+        //transform.LookAt( transform.position + m_Camera.transform.rotation * Vector3.forward ); 
+        //haha ukrylem cos
     }
 
 }
